4be4e955e31b96ef1a15a2f28fef6bb374a1a3ee
hardeepthiara <hardeepkaur.bca@gmail.com>
2016-03-05 18:16:38 +0530

    first commit

--- libsys/admin.py
+++ libsys/admin.py
@@ -1,16 +1,43 @@
 from django.contrib import admin
-from .models import Book, Student
+from libsys.models import * 
+
+
+
+
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 	pass
-	#list_display = ('accno')
+	#list_display=('regno','author',)
+	search_fields=['accno']
+	#list_filter=('regno','accno','title')
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ('name', 'regno','date_of_issue')
+	list_display = ('name', 'regno','date_of_issue','date_of_issue')
+
+class IssueAdmin(admin.ModelAdmin):
+	list_display=('regno','date_of_issue',)
+	search_fields=['date_of_issue']
+	list_filter=('regno','accno','date_of_issue')
+admin.site.register(Issue,IssueAdmin)
+
+
+
+
+class ReturnAdmin(admin.ModelAdmin):
+	list_display=('regno','date_of_return',)
+admin.site.register(Return,ReturnAdmin)
+
+class FineAdmin(admin.ModelAdmin):
+	list_display=('regno','fine',)
+
+admin.site.register(Fine,FineAdmin)
+
+
+
 
 
 
